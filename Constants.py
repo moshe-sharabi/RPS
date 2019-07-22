acc38640b312a93dfd347b61e6af2007dbfd9279
pelegn1 <pelegn1@gmail.com>
2019-07-22 16:00:08 +0300

    added attribute_functions

--- Constants.py
+++ Constants.py
@@ -1,23 +1,39 @@
+from attribute_functions import *
+
 Rock = 'R'
 Paper = 'P'
 Scissors = 'S'
 Choices = [Rock, Paper, Scissors]
 
-PATTERN_S_NEXT_CHOICE = "pattern's next choice"
-LAST_SEQUENCE_S_LENGTH = "last sequence's length"
-PLAYED_AFTER_COMP_MOVE = "most played after <comp move>"
+WIN = 'W'
+DRAW = 'D'
+LOSS = 'L'
+
+points = {
+    WIN: 1,
+    DRAW: 0,
+    LOSS: -1
+}
+
+INDEX_OF_PLAY = 0
+INDEX_OF_RESULT = 1
+
+PATTERN_S_NEXT_CHOICE = "patterns next choice"
+LAST_SEQUENCE_S_LENGTH = "last sequence length"
+MOST_PLAYED_AFTER_COMP_MOVE = "most played after <comp move>"
 MOST_SUCCESSFUL = "most successful"
 MOST_PLAYED_IN_LAST_10 = "most played in last 10"
 MOST_PLAYED = "most played"
 LAST_PLAYED = "last played"
-attribute_names = [LAST_PLAYED, MOST_PLAYED, MOST_PLAYED_IN_LAST_10, MOST_SUCCESSFUL, PLAYED_AFTER_COMP_MOVE, LAST_SEQUENCE_S_LENGTH, PATTERN_S_NEXT_CHOICE]
+attribute_names = [LAST_PLAYED, MOST_PLAYED, MOST_PLAYED_IN_LAST_10, MOST_SUCCESSFUL, MOST_PLAYED_AFTER_COMP_MOVE, LAST_SEQUENCE_S_LENGTH, PATTERN_S_NEXT_CHOICE]
+attribute_functions = [get_last_played, get_most_played, get_most_playedd_in_last_10, get_most_successful, get_most_played_after_comp_move, get_last_sequence_length, get_pattern_next_choice]
 
 parameters = {
     LAST_PLAYED: Choices,
     MOST_PLAYED: Choices,
     MOST_PLAYED_IN_LAST_10: Choices,
     MOST_SUCCESSFUL: Choices,
-    PLAYED_AFTER_COMP_MOVE: Choices,
+    MOST_PLAYED_AFTER_COMP_MOVE: Choices,
     LAST_SEQUENCE_S_LENGTH: [1, 2, 3, 4, '5+'],
     PATTERN_S_NEXT_CHOICE: Choices + ['N']
 
---
+++ attribute_functions.py
@@ -0,0 +1,56 @@
+from util import Counter
+from Constants import *
+
+
+class HistoryException(ValueError):
+    def __init__(self):
+        super(HistoryException, self).__init__()
+
+def get_last_played(history):
+    if not history:
+        raise HistoryException
+    return history[-1][0]
+
+def get_most_played(history):
+    c = Counter()
+    for game in history:
+        c[game[INDEX_OF_PLAY]] += 1
+    return c.argMax()
+
+def get_most_playedd_in_last_10(history):
+    if len(history) <= 10:
+        return get_most_played(history)
+    return get_most_played(history[-10:])
+
+def get_most_successful(history):
+    c = Counter()
+    for game in history:
+        c[game[INDEX_OF_PLAY]] += points[game[INDEX_OF_RESULT]]
+    return c.argMax()
+
+def get_most_played_after_comp_move(history):
+    c = Counter()
+    for i in range(len(history)-1):
+        pass
+    return c.argMax()
+
+def get_last_sequence_length(history):
+    sequence_play = history[-1][INDEX_OF_PLAY]
+    reversed_history = reversed(history)[1:]
+    i = 1
+    for game in reversed_history:
+        if game[INDEX_OF_PLAY] != sequence_play:
+            return i
+        i += 1
+    return i
+
+def get_pattern_next_choice(history):
+    history_no_results = [game[INDEX_OF_PLAY] for game in history]
+    longest_possible_sequence = history/2 if history%2==0 else int(history/2)+1
+    shortest_checked_sequence = 2
+    for i in range(longest_possible_sequence,shortest_checked_sequence,-1): # i is sequence length
+        set1 = history_no_results[1-i*2:1-i]
+        set2 = history_no_results[1-i:]
+        if set1[:-1] == set2:
+            return set1[-1]
+    return
