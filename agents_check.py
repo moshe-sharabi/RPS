7b79189e049470948b385adad3608b3ca6c9ccc9
pelegn1 <pelegn1@gmail.com>
2019-08-14 20:12:50 +0300

    added files to check which agent and which parameters are better

---
+++ agents_check.py
@@ -0,0 +1,48 @@
+from learner import *
+
+folder = AI_agent.example_folder_name
+file1 = os.path.join(folder, "ai_examples.txt")
+file2 = os.path.join(folder, "epoch_examples.txt")
+
+save_to = open(os.path.join('.', 'agent_scores_plus_prob.txt'), 'w')
+
+histories = read_histories(file1)
+histories += read_histories(file2)
+
+for epoch in range(5,11):
+    for _gamma in range(11):
+        gamma = 0.1 * _gamma
+        score1 = 0
+        score2 = 0
+        score_prob1 = 0
+        count = 0
+        for match in histories:
+            agent1 = Ai2(epoch,5,gamma)
+            agent2 = OnlineEpochAgent(epoch,5,gamma)
+            for i in range(len(match)-2):
+                count += 1
+                cur_hist = match[:i]
+                played = match[i][INDEX_OF_PLAY]
+
+                comp_move = agent1.predict(cur_hist).best_counter()
+                game = PAIR_TRANSLATOR[played + comp_move]
+                comp_result = neg[game][INDEX_OF_RESULT]
+                score1 += points[comp_result]
+
+                comp_move = agent1.predict(cur_hist).best_counter_probabilistic()
+                game = PAIR_TRANSLATOR[played + comp_move]
+                comp_result = neg[game][INDEX_OF_RESULT]
+                score_prob1 += points[comp_result]
+
+                comp_move = agent2.predict(cur_hist).best_counter()
+                game = PAIR_TRANSLATOR[played + comp_move]
+                comp_result = neg[game][INDEX_OF_RESULT]
+                score2 += points[comp_result]
+        norm1 = score1 / count
+        norm2 = score2 / count
+        print(f"Ai2({epoch},5,{gamma}) score: {score1}, normalized: {norm1}")
+        save_to.write(f"Ai2({epoch},5,{gamma}) score: {score1}, normalized: {norm1}\n\n")
+        print(f"Ai2_probapilistic({epoch},5,{gamma}) score: {score_prob1}, normalized: {norm1}")
+        save_to.write(f"Ai2_probapilistic({epoch},5,{gamma}) score: {score_prob1}, normalized: {norm1}\n\n")
+        print(f"OnlineEpochAgent({epoch},5,{gamma}) score: {score2}, normalized: {norm2}")
+        save_to.write(f"OnlineEpochAgent({epoch},5,{gamma}) score: {score2}, normalized: {norm2}\n\n")
